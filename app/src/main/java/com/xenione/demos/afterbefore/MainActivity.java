913d2c2f607764daf66d31403d32382defddec94
Eugeni S <esenent@hotmail.com>
2016-10-07 19:36:01 +0200

    Empty Project

---
+++ app/src/main/java/com/xenione/demos/afterbefore/MainActivity.java
@@ -0,0 +1,15 @@
+package com.xenione.demos.afterbefore;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.xenione.demos.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
